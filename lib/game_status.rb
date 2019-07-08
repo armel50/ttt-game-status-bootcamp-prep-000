ec9713bb3bb5b0aa7abf537e7848933338ba1d30
Cloe Kouadjo <armelfli50@gmail.com>
2019-07-08 19:31:57 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,19 +7,23 @@ end
 
 def won(board){
   # row win
-  top_row_win = [0,1,2]
-  middle_row_win = [3,4,5]
-  bottom_row_win = [6,7,8]
+  
+  
+  WIN_COMBINATIONS = [
+    
+          #top_row_win  [0,1,2],
+  #middle_row_win [3,4,5],
+  #bottom_row_win  [6,7,8],
   
   #vertical win
   
-  vertical_left = [0,3,6]
-  vertical_middle = [1,4,7]
-  vertical_right = [2,5,8]
+  #vertical_left  [0,3,6],
+  #vertical_middle [1,4,7],
+  #vertical_right =[2,5,8],
   
   #diagnols
-  first_diagnols = [0,4,8]
-  second_diagnols = [2,4,6]
-  
-  while
+  #first_diagnols [0,4,8],
+  #second_diagnols  [2,4,6],
+    
+    ]
 }
