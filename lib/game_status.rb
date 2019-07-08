0b50f5af056e78e3392fcebb3d7ffa388c08eee4
Cloe Kouadjo <armelfli50@gmail.com>
2019-07-08 20:40:43 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,14 +7,14 @@ end
 
  WIN_COMBINATIONS = [
     
-      [0,1,2], #top_row_win  
-      [3,4,5],  #middle_row_win
-      [6,7,8],  #bottom_row_win
-      [0,3,6], #vertical_left 
-      [1,4,7],  #vertical_middle
-      [2,5,8], #vertical_right
-      [0,4,8], #first_diagnols 
-      [2,4,6] #second_diagnols 
+      [0,1,2],
+      [3,4,5],  
+      [6,7,8], 
+      [0,3,6], 
+      [1,4,7], 
+      [2,5,8], 
+      [0,4,8],
+      [2,4,6] 
     ]
  def won?(board)
    contains_X = board.include?("X")
