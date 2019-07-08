eb81f1205b4aed6ff3b1ebfef2d563623742a481
Cloe Kouadjo <armelfli50@gmail.com>
2019-07-08 19:34:55 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,11 +5,7 @@ end
 
 # Define your WIN_COMBINATIONS constant
 
-def won(board){
-  
-  
-  
-  WIN_COMBINATIONS = [
+ WIN_COMBINATIONS = [
     
       [0,1,2],#top_row_win  
       [3,4,5],  #middle_row_win
@@ -20,4 +16,9 @@ def won(board){
       [0,4,8], #first_diagnols 
       [2,4,6],#second_diagnols 
     ]
+def won(board){
+  
+  
+  
+ 
 }
